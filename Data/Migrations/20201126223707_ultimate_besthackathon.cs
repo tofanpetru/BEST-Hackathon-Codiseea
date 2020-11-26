28f4329114e6ff5b07864217854788c246918966
tofanpetru <tofanpetru12@gmail.com>
2020-11-27 00:41:04 +0200

    minor updates

---
+++ Data/Migrations/20201126223707_ultimate_besthackathon.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Best_Hackathon_Codiseea.Data.Migrations
+{
+    public partial class ultimate_besthackathon : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
